47acf0431344a7a642ef4a8f87d67bf57530b66a
wwwtfden <denn.tdi@gmail.com>
2022-09-09 15:30:16 +0300

    Creating first verison

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,92 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.Text;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch; //область отрисовки объектов
+	//Texture img; //текстура, какая-то
+	Background bg;
+	Bird bird;
+	Obstacles obstacles;
+	boolean gameOver;
+	Texture restartTexture;
+
+	int pointCounter = 0;
+	
+	@Override
+	public void create () { //запускается один раз, подгружает все необходимые элементы.
+		batch = new SpriteBatch();
+		bg = new Background();
+		bird = new Bird();
+		obstacles = new Obstacles();
+		gameOver = false;
+		restartTexture = new Texture("RestartBtn.png");
+	}
+
+	@Override
+	public void render () { //выз каждый кадр
+		update(); //пересчет математики для всей игры
+		ScreenUtils.clear(1, 1, 1, 1); //заливка всего игрового поля каким-то цветом (RGB + альфа-канал)
+		batch.begin(); //начало отрисовки
+		//batch.draw(img, 0, 0); //отрисовка фоновой картинки с параметрами начальных координат (с левого НИЖНЕГО угла)
+		bg.render(batch); //рендерим бэкграунд при помощи batch
+		obstacles.render(batch);
+		if (!gameOver){
+			bird.render(batch); //также рендерим птицу
+		} else {
+			batch.draw(restartTexture, 200,200);
+			System.out.println("COMPLETE! MI PROLETELI: " + pointCounter);
+		}
+
+		batch.end();
+	}
+
+	//функция, в которой мы будем хранить и изменять положение картинки с обновлением
+	public void update(){
+		bg.update();
+		bird.update();
+		obstacles.update();
+		for (int i = 0; i < Obstacles.obs.length; i++) {
+			if (bird.position.x > Obstacles.obs[i].position.x && bird.position.x < Obstacles.obs[i].position.x + 50){
+				if (!Obstacles.obs[i].emptySpace.contains(bird.position)){
+					gameOver = true;
+				}
+			}
+		}
+		if (bird.position.y < 0 || bird.position.y > 600){
+			gameOver = true;
+		}
+
+
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && gameOver){
+			recreate();
+		}
+		for (int i = 0; i < 4; i++) {
+			if (bird.position.x == obstacles.obs[i].position.x + 50){
+				//System.out.println("PROLETELI NAHUI " + i);
+				pointCounter++;
+				System.out.println("PROLETELI NAHUI " + pointCounter);
+			}
+		}
+
+	}
+	
+	@Override
+	public void dispose () { //очистка ресурсов
+		batch.dispose(); //типа удаляем...
+		//img.dispose();
+	}
+
+	public void recreate(){
+		pointCounter = 0;
+		bird.recreate();
+		obstacles.recreate();
+		gameOver = false;
+	}
+}
